c14ba8e83319b2096363a9782431f3e547e0b6a2
Naimul Sakib <58085184+NaimulSakib@users.noreply.github.com>
2021-06-27 19:55:07 +0600

    Add files via upload

---
+++ URI1.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+x = a + b                             # uri 1001 problem solved    %i is for int %d is for desimel and %f is for flote and %x is for veriables
+print("X = %i"%x)
