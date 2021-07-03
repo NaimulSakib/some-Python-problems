72a19fe72f7f305a4489f9f839f0d3b06fa1d198
Naimul Sakib <58085184+NaimulSakib@users.noreply.github.com>
2021-07-03 14:14:14 +0600

    Add files via upload

---
+++ URI8.py
@@ -0,0 +1,7 @@
+
+NUMBERS=int(input())
+hourse=int(input())
+salary=float(input())             # 1008 URI this is worked but thay did not ecsept it # fuck it was NUMBER not NUMBERS my bad .....
+SALARY=(hourse*salary)
+print("NUMBER = %i"%NUMBERS)
+print("SALARY = U$ %.2f"%SALARY)
